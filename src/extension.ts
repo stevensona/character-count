2e72e1205c554f27e44f235b3345ae64cf9bb2fe
stevensona <adamstevenson121@gmail.com>
2016-07-15 16:18:21 -0400

    remove message

--- src/extension.ts
+++ src/extension.ts
@@ -7,10 +7,6 @@ import {window, workspace, commands, Disposable, ExtensionContext, StatusBarAlig
 // your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "character-count" is now active!');
-
     let characterCounter = new CharacterCounter();
     let controller = new CharacterCounterController(characterCounter);
     context.subscriptions.push(controller);
